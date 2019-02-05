d2add2dadd8be4912ca4e30c8a9bae892b55d5c7
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-02-05 10:08:41 -0800

    Create variable_declaration_2.cpp

---
+++ variable/variable declaration/variable_declaration_2.cpp
@@ -0,0 +1,29 @@
+/*
+There are seven (7) ways to declare variable . This second way to declare variable.
+*/
+
+
+#include <iostream>
+
+using namespace std ;
+
+int main()
+
+{
+
+   int inumber ;                                   // declaration of variable .
+
+   inumber=(10);							                		// Initialization of variable .
+   
+  cout << inumber << endl; 						
+	cout << &inumber << endl;                      // give address of variable .
+
+	return 0;
+
+}
+
+/*
+OUTPUT
+10
+0x23fe1c
+*/

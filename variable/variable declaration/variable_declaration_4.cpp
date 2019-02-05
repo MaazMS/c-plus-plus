f7c3ae1edf81a32b6256046327730c5ddac7e78b
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-02-05 10:33:11 -0800

    Create variable_declaration_4.cpp

---
+++ variable/variable declaration/variable_declaration_4.cpp
@@ -0,0 +1,26 @@
+There are seven (7) ways to declare variable . This fourth way to declare variable.
+*/
+
+#include <iostream>
+
+using namespace std ;
+
+int main()
+
+{
+  auto int inumber;                 // warning: 'auto' changes meaning in C++11; please remove it [-Wc++0x-compat]
+	
+  inumber=10;                      // Initialization of variable .
+  
+  cout << inumber << endl; 						
+  cout << &inumber << endl;
+
+	return 0;
+
+}
+/*
+OUTPUT
+10
+0x23fe1c
+
+*/

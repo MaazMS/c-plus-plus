d8c3e6deaa16c6067b3eb48621b6dfefe83b0671
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-02-14 19:12:12 -0800

    Create default_constructor2.cpp

---
+++ constructor/default_constructor/default_constructor2.cpp
@@ -0,0 +1,54 @@
+//  The user is not assign the value of data member for default constructor the is print garbage value .
+
+#include<iostream>
+
+using namespace std;
+
+
+class default_constructor
+{
+
+private :
+
+	int number;
+
+
+public:
+
+	default_constructor()						                        // warning: statement has no effect [-Wunused-value]
+	{
+
+		number;								                               // warning: statement has no effect [-Wunused-value]r
+
+	}
+  
+  
+	void print()
+	{
+		cout << "value of number= "<<number<<endl;          /* warning: 'object.default_constructor::number'
+												                                is used uninitialized in this function [-Wuninitialized]
+		 	 	 	 	 	 	 	 	 	 	                                  */
+
+		cout << "value of number= "<<&number<<endl;
+	}
+
+
+};
+
+
+int main()
+{
+
+	default_constructor object;
+
+	object.print();
+
+
+	return 0 ;
+}
+/*
+OUTPUT
+value of number= 4198400
+value of number= 0x23fe10
+
+*/

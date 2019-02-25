7672a60ec5b34ce9a185b4459f971fd008a4b112
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-02-24 21:35:03 -0800

    Create Parameterized_constructor_Explicit_call.cpp

---
+++ constructor/Parameterized_constructor/Parameterized_constructor_Explicit_call.cpp
@@ -0,0 +1,56 @@
+ #include<iostream>
+
+using namespace std;
+
+
+class parameterized_constructor
+{
+
+private:
+
+
+	int first_number,second_number;
+
+public:
+
+
+	parameterized_constructor(int number1,int number2)             //parameterized_constructor function
+	{
+
+		first_number=number1;
+		second_number=number2;
+
+	}
+
+
+	void display()
+	{
+
+		cout<<"first number is = "<<first_number<<endl;
+
+		cout<<"second number is ="<<second_number<<endl;
+
+	}
+
+};
+
+
+int main()
+{
+
+	 // Explicit call of parameterized_constructor to passing the argument through object .
+
+
+	parameterized_constructor object=parameterized_constructor(10,20);
+
+	object.display();
+
+	return 0;
+
+}
+/*
+ * OUTPUT
+first number is = 10
+second number is =20
+ *
+ */
